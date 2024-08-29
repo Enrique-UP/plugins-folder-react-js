fe3bb96b6a7fe9c3b1a1c8b6b370737a02801421
noman.na <noman.na@I2A-PC-100>
2024-08-29 15:01:46 +0530

    form

--- react-forms/one/src/App.js
+++ react-forms/one/src/App.js
@@ -1,18 +1,9 @@
-// import FormArea from "./forms/FormArea";
 import Basic from "./forms/Basic";
-// import FormArea1 from "./forms/FormArea1";
-
 
 const App = () => {
   return(
     <>
       <Basic />
-      
-      {/* <br /><br /><br /><br /><br />
-      <FormArea /> */}
-      
-      {/*<br /><br /><br /><br /><br />
-      <FormArea1 /> */}
     </>
   )
 }
--- react-forms/one/src/forms/Basic.js
+++ react-forms/one/src/forms/Basic.js
@@ -1,68 +1,112 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-
-import { useFormik, Formik, Form } from "formik";
+import { useFormik } from "formik";
 import * as Yup from 'yup';
 
 const FormValidation = Yup.object({
-  user_name: Yup.string().min(3).required("Please enter name."),
-  user_email: Yup.string().email("Please enter valid email.").required("Please enter valid email."),
-  user_password: Yup.string().min(5).required("Please enter password."),
-  user_cpassword: Yup.string().oneOf([Yup.ref("password")], "Password not matched.").required("Please enter confirm password.")
+  user_name: Yup.string().min(3, "Name must be at least 3 characters long.").required("Please enter your name."),
+  user_email: Yup.string()
+    .email("Please enter a valid email address.")
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please enter a valid email address."
+    )
+    .required("Please enter your email."),
+  country_code: Yup.string()
+    .matches(/^\+\d{1,3}$/, "Please enter a valid country code (e.g., +1, +44 etc.).")
+    .required("Please enter your country code."),
+  user_phone: Yup.string()
+    .matches(/^[0-9]{3,20}$/, "Please enter a valid phone number.")
+    .required("Please enter your phone number."),
+  user_message: Yup.string().min(10, "Message must be at least 10 characters long."),
 });
 
 const initialValues = {
-  name:"",
-  email:"",
-  password:"",
-  cpassword:""
+  user_name: "",
+  user_email: "",
+  country_code: "",
+  user_phone: "",
+  user_message: "",
 }
 
 function FormArea() {
-  const {values, handleBlur, handleChange, handleSubmit, errors} = useFormik({
+  const form = useRef();
+
+  const formik = useFormik({
     initialValues: initialValues,
-    validationSchema:FormValidation,
-    onSubmit:(values) => {
-      console.log(values);
+    validationSchema: FormValidation,
+    onSubmit: (values, { resetForm }) => {
+      emailjs.sendForm('service_naawjgt', 'template_a4uikdp', form.current, 'pMOGs9W_LfCQfWE8N')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+      resetForm();
     }
   });
 
-  const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_naawjgt', 'template_a4uikdp', form.current, 'pMOGs9W_LfCQfWE8N')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
-  };
-  
   return (
     <>
-      <Formik initialValues={initialValues} validationSchema={FormValidation}>
-        <Form ref={form} onSubmit={(f) => {sendEmail(f); handleSubmit(f)}}>
-          <label htmlFor="name">Name</label><br />
-          <input type="text" name="user_name" value={values.user_name} onBlur={handleBlur} onChange={handleChange} /><br />
-          {errors.user_name && <small>{errors.user_name}</small>}<br />
+      <form ref={form} onSubmit={formik.handleSubmit}>
+        <label htmlFor="user_name">Name</label><br />
+        <input 
+          type="text" 
+          name="user_name" 
+          value={formik.values.user_name} 
+          onBlur={formik.handleBlur} 
+          onChange={formik.handleChange} 
+          title="Name must be at least 3 characters."
+        /><br />
+        {formik.errors.user_name && formik.touched.user_name && <small>{formik.errors.user_name}</small>}<br />
 
-          <label htmlFor="email">Email</label><br />
-          <input type="email" name="user_email" value={values.user_email} onBlur={handleBlur} onChange={handleChange} /><br />
-          {errors.user_email && <small>{errors.user_email}</small>}<br />
+        <label htmlFor="user_email">Email</label><br />
+        <input 
+          type="email" 
+          name="user_email" 
+          value={formik.values.user_email} 
+          onBlur={formik.handleBlur} 
+          onChange={formik.handleChange} 
+          pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" 
+          title="Please enter a valid email address." 
+        /><br />
+        {formik.errors.user_email && formik.touched.user_email && <small>{formik.errors.user_email}</small>}<br />
 
-          <label htmlFor="password">Password</label><br />
-          <input type="password" name="user_password" value={values.user_password} onBlur={handleBlur} onChange={handleChange} /><br />
-          {errors.user_password && <small>{errors.user_password}</small>}<br />
+        <label htmlFor="country_code">Country Dialing Code</label><br />
+        <input 
+          type="text" 
+          name="country_code" 
+          value={formik.values.country_code} 
+          onBlur={formik.handleBlur} 
+          onChange={formik.handleChange} 
+          pattern="^\+\d{1,3}$" 
+          title="Please enter a valid country code (e.g., +1, +44 etc.)." 
+        /><br />
+        {formik.errors.country_code && formik.touched.country_code && <small>{formik.errors.country_code}</small>}<br />
+        
+        <label htmlFor="user_phone">Phone Number</label><br />
+        <input 
+          type="text" 
+          name="user_phone" 
+          value={formik.values.user_phone} 
+          onBlur={formik.handleBlur} 
+          onChange={formik.handleChange} 
+          pattern="[0-9]{3,20}" 
+          title="Please enter a valid phone number with 3 to 20 digits." 
+        /><br />
+        {formik.errors.user_phone && formik.touched.user_phone && <small>{formik.errors.user_phone}</small>}<br />
 
-          <label htmlFor="cpassword">Confirm Password</label><br />
-          <input type="password" name="user_cpassword" value={values.user_cpassword} onBlur={handleBlur} onChange={handleChange} /><br />
-          {errors.user_cpassword && <small>{errors.user_cpassword}</small>}<br />
+        <label htmlFor="user_message">Message</label><br />
+        <textarea 
+          name="user_message" 
+          value={formik.values.user_message} 
+          onBlur={formik.handleBlur} 
+          onChange={formik.handleChange} 
+        /><br />
+        {formik.errors.user_message && formik.touched.user_message && <small>{formik.errors.user_message}</small>}<br />
 
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
